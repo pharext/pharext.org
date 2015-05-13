2f593903bb0b095e3ecaece6c8e73ab3410af64f
Michael Wallner <mike@php.net>
2015-05-13 10:47:04 +0200

    bail out if config cannot be parsed

--- app/bootstrap/config.php
+++ app/bootstrap/config.php
@@ -7,11 +7,18 @@ define("APP_ENVIRONMENT", getenv("APP_ENVIRONMENT") ?: "localhost");
 $injector->share(Config::class)
 	->define(Config::class, [
 		"+array" => function($key, $injector) {
-			return parse_ini_file(__DIR__."/../../config/app.ini", true);
+			$settings = parse_ini_file(__DIR__."/../../config/app.ini", true);
+			if (!$settings) {
+				throw new \Exception("Could not parse settings");
+			}
+			return $settings;
 		},
 		":section" => APP_ENVIRONMENT
 	])
 	->prepare(Config::class, function($config, $injector) {
 		$credentials = parse_ini_file(__DIR__."/../../config/credentials.ini", true);
+		if (!$credentials) {
+			throw new \Exception("Could not parse credentials");
+		}
 		$config->addConfig(new Config($credentials, APP_ENVIRONMENT));
 	});
